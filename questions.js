2860526cfed50bbb768a088cde446d5809c1548d
Shivani Nandane <129670498+ShivaniNandane@users.noreply.github.com>
2023-04-15 04:56:14 -0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,117 @@
+//creating an array and passing the number , questions , options , and answers
+let questions = [{
+        numb: 1,
+        questions: "Which of the following is not  a top-level container?",
+        answer: "Jpanel",
+        options: [
+            "Jpanel",
+            "Jframe",
+            "Jwindow",
+            "Jdialog",
+        ]
+    },
+    {
+        numb: 2,
+        questions: "What is the default state of checkbox?",
+        answer: "unchecked",
+        options: [
+            "unchecked",
+            "checked",
+            "Both 1 and 2",
+            "None of the above",
+        ]
+    },
+
+    {
+        numb: 3,
+        questions: "The two main features pf Swing are-----and----",
+        answer: "top level and light weight",
+        options: [
+            "top level and light weight",
+            "top level",
+            "container",
+            "components",
+        ]
+    },
+
+    {
+        numb: 4,
+        questions: "What is used to execute parametrized query?",
+        answer: "PreparedStatement interface",
+        options: [
+            "Statement inerface",
+            "PreparedStatement interface",
+            "ResultSet interface",
+            "None Of the above",
+        ]
+    },
+    {
+        numb: 5,
+        questions: "JDBC Stands for?",
+        answer: "Java Database Connectivity",
+        options: [
+            "Java Database Connectivity",
+            "Java Database Concept",
+            "Java Database Communications",
+            "None Of the above",
+        ]
+    },
+    {
+        numb: 6,
+        questions: "How many tyes  of  control does AWT is support?",
+        answer: "7",
+        options: [
+            "7",
+            "6",
+            "5",
+            "8",
+        ]
+    },
+    {
+        numb: 7,
+        questions: "For which puropse is the Inet Address class used?",
+        answer: "IP Address",
+        options: [
+            "ip Address",
+            "Socket",
+            "TCP/IP",
+            "none of these",
+        ]
+    },
+
+    {
+        numb: 8,
+        questions: "What is the Lifecycle of a Servlet?",
+        answer: "All mentioned above",
+        options: [
+            "Servlet class is loaded",
+            "Servlet instance is created",
+            "init,Service,destroy method is invoked",
+            "All mentioned above",
+        ]
+    },
+
+    {
+        numb: 9,
+        questions: "Which tag is used to execute java source code in JSP?",
+        answer: "Scriptlet tag",
+        options: [
+            "Declaration Tag",
+            "Scriptlet tag",
+            "Expression tag",
+            "None of the above",
+        ]
+    },
+
+    {
+        numb: 10,
+        questions: "Which packages does a JSP API consist?",
+        answer: "Both A and C ",
+        options: [
+            "javax.servlet.jsp",
+            "java.servlet",
+            "javax.servlet.jsp.tagext",
+            "Both A and C",
+        ]
+    },
+];
